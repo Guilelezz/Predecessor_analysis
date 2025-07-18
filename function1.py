1921fb08ecb072dfcfb2ce51dab299d6ddeeda1e
Thomas.N <tnielsen2810@gmail.com>
2025-07-18 02:06:59 +0200

    Cleaning up files
    Cleaned up unused files as well as temporary files that should have been deleted.
    Added .gitignore for putting local test files into, as well as temporary outputs.

--- function1.py
+++
@@ -1,6 +0,0 @@
-# function1.py
-def add(a: int, b: int):
-    return a + b
-
-def multiply(a: int, b: int):
-    return a * b
--- function2.py
+++
@@ -1,6 +0,0 @@
-# function2.py
-def greet(name: str):
-    return f"Hello, {name}!"
-
-def square(x: int):
-    return x ** 2
